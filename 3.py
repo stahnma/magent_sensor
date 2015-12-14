f1b595ac4ff02173bcbec9473725aea3ef32ce17
Michael Stahnke <stahnma@websages.com>
2015-12-14 06:10:16 +0000

    Working for state change

--- 3.py
+++ 3.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
  
 import RPi.GPIO as GPIO 
 import time
@@ -9,23 +8,27 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 fridge = 12
-
 log = open('fridge.log', 'a')
+laststate = 'closed'
 
 GPIO.setup(fridge, GPIO.IN, GPIO.PUD_UP)
 
 while True:
    if GPIO.input(fridge) == False:
-       ts = time.time() 
-       dt = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-       log.write(ts + "," + dt + "," + "closed\n")
-       print(dt + " - Refridgerator 1 is open.")
+       if laststate == "closed":
+	 laststate = 'open'
+	 ts = time.time() 
+	 dt = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+	 log.write(str(dt) + "," + str(ts) + "," + "open\n")
+	 print(dt + " - Refridgerator door is open.")
        time.sleep(2)
    else:
      if GPIO.input(fridge) == True:
-       ts = time.time() 
-       dt = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-       log.write(str(dt) + "," + str(ts) + "," + "closed.\n")
-       print(dt + " - Door is now closed.")
+       if laststate == "open":
+	 laststate = 'closed'
+	 ts = time.time() 
+	 dt = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+	 log.write(str(dt) + "," + str(ts) + "," + "closed\n")
+	 print(dt + " Refridgerator door is closed.")
        time.sleep(2)       
 GPIO.cleanup()
