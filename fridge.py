8c131894fb7aaaff41c126e8bbdf1440e202773f
Michael Stahnke <stahnma@websages.com>
2016-01-31 16:36:10 -0600

    Add logging to STDOUT if DEBUG=1
    Conflicts:
    	fridge.py

--- fridge.py
+++ fridge.py
@@ -4,6 +4,7 @@ import RPi.GPIO as GPIO
 import time
 import datetime
 import os
+import sys
 
 GPIO.setmode(GPIO.BCM) 
 GPIO.setwarnings(False)
@@ -15,12 +16,14 @@ laststate = 'closed'
 GPIO.setup(fridge, GPIO.IN, GPIO.PUD_UP)
 
 while True:
+   sys.stdout.flush()
    if GPIO.input(fridge) == False:
        ts = time.time() 
        dt = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
        if laststate == "closed":
 	 laststate = 'open'
 	 log.write(str(dt) + "," + str(ts) + "," + "open\n")
+         log.flush()
 	 print(str(dt) + "," + str(ts) + "," + "fridge open")
        if os.environ.get('DEBUG'):
          print(str(dt) + "," + str(ts) + "," + "fridge open")
@@ -32,6 +35,7 @@ while True:
        if laststate == "open":
 	 laststate = 'closed'
 	 log.write(str(dt) + "," + str(ts) + "," + "closed\n")
+         log.flush()
 	 print(str(dt) + "," + str(ts) + "," + "fridge closed")
        if os.environ.get('DEBUG'):
          print(str(dt) + "," + str(ts) + "," + "fridge closed")
